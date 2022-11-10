ebb36f5e9fbc54e8cdfcbed06659584b94428003
Victor <victorrebeque@gmail.com>
2022-11-10 16:35:32 -0300

    iniciando Js com requisições http em uma api

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+
+
+fetch(url)
+.then((response) =>  response.json())
+.then((jsonBody) => {
+    console.log(jsonBody)
+})
+.catch((err) => {
+    console.log(err)
+})
+.finally(() => {
+    console.log('requisição concluída!')
+})
