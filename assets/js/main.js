7208baef06c24005424d6e22aedd7d1b83dab75e
Victor <victorrebeque@gmail.com>
2022-11-10 18:33:20 -0300

    Alimentando html usando js com informacoes da api

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,33 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
 
+function convertPokemonToHtml (pokemon) {
+    return `
+<li class="pokemon">
+    <span class="number">#001</span>
+    <span class="name">${pokemon.name}</span>
+
+    <div class="detail">
+        <ol class="types">
+            <li class="type">grass</li>
+            <li class="type">poison</li>
+        </ol>
+        <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" alt="${pokemon.name}">
+    </div>
+</li>
+`
+}
+
+const pokemonList = document.getElementById('pokemonList');
 
 fetch(url)
 .then((response) =>  response.json())
-.then((jsonBody) => {
-    console.log(jsonBody)
+.then((jsonBody) => jsonBody.results)
+.then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        console.log(convertPokemonToHtml(pokemon))
+        pokemonList.innerHTML+= convertPokemonToHtml(pokemon)
+    }
 })
-.catch((err) => {
-    console.log(err)
-})
-.finally(() => {
-    console.log('requisição concluída!')
-})
+.catch((err) => console.log(err))
